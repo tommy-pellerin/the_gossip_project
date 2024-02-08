b0fe10499a1bb6a75944707cbfda4519cad2e391
tommy-pellerin <tommytp@hotmail.fr>
2024-02-08 18:28:53 +0100

    fin Q f)messages privés

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_155941) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_161711) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.string "zip_code"
@@ -34,6 +34,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_155941) do
     t.datetime "updated_at", null: false
     t.index ["gossip_id"], name: "index_join_tag_with_gossips_on_gossip_id"
     t.index ["tag_id"], name: "index_join_tag_with_gossips_on_tag_id"
+  end
+
+  create_table "private_messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "recipient_id"
+    t.integer "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipient_id"], name: "index_private_messages_on_recipient_id"
+    t.index ["sender_id"], name: "index_private_messages_on_sender_id"
   end
 
   create_table "tags", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,20 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 require "faker"
-# 10.times do 
-#   city = City.create!(name:Faker::Address.city, zip_code:Faker::Address.zip_code)
-# end
-# User.destroy_all
-# cities = City.all
-# 10.times do 
-#   user = User.create!(first_name: Faker::Name.first_name, last_name:Faker::Name.last_name, description:Faker::Superhero.descriptor, email:Faker::Internet.email, age:Faker::Number.decimal_part(digits: 2), city:cities.sample)
-# end
+10.times do 
+  city = City.create!(name:Faker::Address.city, zip_code:Faker::Address.zip_code)
+end
+User.destroy_all
+cities = City.all
+10.times do 
+  user = User.create!(first_name: Faker::Name.first_name, last_name:Faker::Name.last_name, description:Faker::Superhero.descriptor, email:Faker::Internet.email, age:Faker::Number.decimal_part(digits: 2), city:cities.sample)
+end
 
-# Gossip.destroy_all
-# users = User.all
-# 20.times do
-#   gossip = Gossip.create!(title:Faker::Movie.title , content:Faker::Quote.yoda , user: users.sample)
-# end
+Gossip.destroy_all
+users = User.all
+20.times do
+  gossip = Gossip.create!(title:Faker::Movie.title , content:Faker::Quote.yoda , user: users.sample)
+end
 
 Tag.destroy_all
 10.times do
@@ -32,5 +32,12 @@ tags = Tag.all
 gossips = Gossip.all
 100.times do
   jointagwithgossip = JoinTagWithGossip.create!(tag:tags.sample, gossip:gossips.sample)
+end
+
+#Private message
+PrivateMessage.destroy_all
+users = User.all
+50.times do
+  privatemessage = PrivateMessage.create!(content: Faker::Quotes::Shakespeare.as_you_like_it_quote, sender: users.sample, recipient: users.sample)
 end
 
